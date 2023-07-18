902ea2c56c9122c39aabde7d62624d28490af1b4
Motoronto <josephaaakoto@gmail.com>
2023-07-18 16:01:02 -0700

    0x0D. C - Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file it was compiled from, followed by a new line.
+ * main - prints the name of the file it was compiled from
  *
- * Return: Always 0 (Successful)
+ * Return: Everything worked ok
  */
 
 int main(void)
